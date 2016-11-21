68cdb8dbeaf67263f3927dff103478678edd36aa
Alex Beltyukov <beltyukov.1@gmail.com>
2016-11-21 00:04:15 -0500

    Implement efficient document

--- src/main/java/document/DocumentBenchmarking.java
+++ src/main/java/document/DocumentBenchmarking.java
@@ -33,32 +33,32 @@ public class DocumentBenchmarking {
 		// THe number of characters to start with. 
 		// You can play around with this.
 		int start = 50000;
-		
-		// TODO: Fill in the rest of this method so that it runs two loops
-		// and prints out timing results as described in the assignment 
-		// instructions and following the pseudocode below.
+
+		System.out.println("NumberOfChars\tBasicTime\tEfficientTime");
+
 		for (int numToCheck = start; numToCheck < numSteps*increment + start; 
 				numToCheck += increment)
 		{
-			// numToCheck holds the number of characters that you should read from the 
-			// file to create both a BasicDocument and an EfficientDocument.  
-			
-			/* Each time through this loop you should:
-			 * 1. Print out numToCheck followed by a tab (\t) (NOT a newline)
-			 * 2. Read numToCheck characters from the file into a String
-			 *     Hint: use the helper method below.
-			 * 3. Time a loop that runs trials times (trials is the variable above) that:
-			 *     a. Creates a BasicDocument 
-			 *     b. Calls fleshScore on this document
-			 * 4. Print out the time it took to complete the loop in step 3 
-			 *      (on the same line as the first print statement) followed by a tab (\t)
-			 * 5. Time a loop that runs trials times (trials is the variable above) that:
-			 *     a. Creates an EfficientDocument 
-			 *     b. Calls fleshScore on this document
-			 * 6. Print out the time it took to complete the loop in step 5 
-			 *      (on the same line as the first print statement) followed by a newline (\n) 
-			 */  
-			 
+			System.out.print(numToCheck + "\t");
+
+			String input = getStringFromFile(textfile, numToCheck);
+
+			long startTime = System.nanoTime();
+            for (int i = 0; i < trials; i++) {
+                Document basicDocument = new BasicDocument(input);
+                basicDocument.getFleschScore();
+            }
+			long endTime = System.nanoTime();
+			System.out.print((endTime - startTime) / 1000000000.0 + "\t");
+
+			startTime = System.nanoTime();
+            for (int i = 0; i < trials; i++) {
+                Document efficientDocument = new EfficientDocument(input);
+                efficientDocument.getFleschScore();
+            }
+			endTime = System.nanoTime();
+
+			System.out.println((endTime - startTime) / 1000000000.0);
 		}
 	
 	}
