385518509c019e49a3176191ede4fbc67a26c029
Alex Beltyukov <alexbeltyukov@carfax.com>
2016-12-01 11:22:31 -0500

    Implement dictionary using linked list and binary search tree

--- src/main/java/spelling/DictionaryBST.java
+++ src/main/java/spelling/DictionaryBST.java
@@ -6,36 +6,38 @@ import java.util.TreeSet;
  * @author UC San Diego Intermediate MOOC team
  *
  */
-public class DictionaryBST implements Dictionary 
-{
+public class DictionaryBST implements Dictionary {
+
    private TreeSet<String> dict;
-	
-    // TODO: Implement the dictionary interface using a TreeSet.  
- 	// You'll need a constructor here
-	
-    
+
+    public DictionaryBST() {
+        dict = new TreeSet<>();
+    }
+
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        if (word == null) {
+            throw new NullPointerException();
+        }
+
+        return !isWord(word) && dict.add(word.toLowerCase());
     }
 
-
     /** Return the number of words in the dictionary */
-    public int size()
-    {
-    	// TODO: Implement this method
-        return 0;
+    public int size() {
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-    	//TODO: Implement this method
-        return false;
-    }
+        if (s == null) {
+            throw new NullPointerException();
+        }
 
+        return dict.contains(s.toLowerCase());
+    }
 }
